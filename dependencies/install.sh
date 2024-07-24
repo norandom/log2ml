47693e1f899d207863a918be2222df512cee9d61
Marius Ciepluch <11855163+norandom@users.noreply.github.com>
2024-07-24 14:32:40 +0200

    Update install.sh

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -4,7 +4,7 @@ if [ -d "/content" ]; then
   echo "This appears to be a Google Colab environment."
 
   echo "Installing dependencies"
-  pip install -r "https://raw.githubusercontent.com/norandom/project_bookworm/main/requirements.gpu.txt"
+  pip install -r "https://raw.githubusercontent.com/norandom/log2ml/main/dependencies/requirements.gpu.txt"
 
 else
   echo "This does not appear to be a Google Colab environment."
