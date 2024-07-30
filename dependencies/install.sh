e98fe3fd5349da0a3ce7c2e77e983d777532d760
Marius Ciepluch <11855163+norandom@users.noreply.github.com>
2024-07-30 12:08:23 +0200

    auto install rapids for gpu acceleration on tpot-nn

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -6,6 +6,10 @@ if [ -d "/content" ]; then
   echo "Installing dependencies"
   pip install -r "https://raw.githubusercontent.com/norandom/log2ml/main/dependencies/requirements.gpu.txt"
 
+  # https://docs.rapids.ai/deployment/stable/platforms/colab/
+  git clone https://github.com/rapidsai/rapidsai-csp-utils.git
+  python rapidsai-csp-utils/colab/pip-install.py
+
 else
   echo "This does not appear to be a Google Colab environment."
 fi
